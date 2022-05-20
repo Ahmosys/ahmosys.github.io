ae3fe0495adada174389868dc1420f1b18f615f5
Ahmosys <ahmosyspro@protonmail.com>
2022-05-20 14:44:21 +0200

    ✨ Edited number of cards for github projects

--- js/script_api_github.js
+++ js/script_api_github.js
@@ -12,31 +12,28 @@ $(document).ready(function() {
     };
 
     $.ajax(settings).done(function (response) {
-        for(let i = 1; i < response.length; i++) {
-            if (i <= 6) {
-                var repoFullName = response[i]["full_name"];
-                var repoName = response[i]["name"];
-                var repoDescription = response[i]["description"];
-                var repoLanguage = response[i]["language"];
-                var repoUrl = response[i]["html_url"];
-                var repoPublishDate = response[i]["updated_at"];
-                var repoDate = new Date(repoPublishDate).toDateString();
-                var isFork = response[i]["fork"];
-    
-                if (repoLanguage == "Visual Basic .NET") {
-                    repoLanguage = "VB.NET";
-                } else if (repoLanguage == "JavaScript") {
-                    repoLanguage = "JS"
-                }
-                if (!isFork) {
-                    $("#repository").append(
-                        "<div class='col-sm-12 col-md-6 col-lg-4 mt-4 d-flex align-items-stretch'><div class='card border-info'><div class='card-body'><div class='d-flex justify-content-between mb-2'><h6 class='card-subtitle text-warning text-uppercase'>" + repoLanguage + "</h6><h6 class='card-subtitle text-warning'>" +  repoDate + "</h6></div><a href='" + repoUrl + "' target='_blank' rel='noreferrer'><h5 class='card-title text-white'>" + repoName + "</h5></a><p class='card-text mt-2'>" + repoDescription + "</p></div></div></div>"
-                    );
-                } else {
-                    break;
-                }
+        let i = 0;
+        while(i <= 6 && response.length <= i) {
+            let repoName = response[i]["name"];
+            let repoDescription = response[i]["description"];
+            let repoLanguage = response[i]["language"];
+            let repoUrl = response[i]["html_url"];
+            let repoPublishDate = response[i]["updated_at"];
+            let repoDate = new Date(repoPublishDate).toDateString();
+            let isFork = response[i]["fork"];
+
+            if (repoLanguage == "Visual Basic .NET") {
+                repoLanguage = "VB.NET";
+            } else if (repoLanguage == "JavaScript") {
+                repoLanguage = "JS"
             }
-            
+
+            if (!isFork) {
+                $("#repository").append(
+                    "<div class='col-sm-12 col-md-6 col-lg-4 mt-4 d-flex align-items-stretch'><div class='card border-info'><div class='card-body'><div class='d-flex justify-content-between mb-2'><h6 class='card-subtitle text-warning text-uppercase'>" + repoLanguage + "</h6><h6 class='card-subtitle text-warning'>" +  repoDate + "</h6></div><a href='" + repoUrl + "' target='_blank' rel='noreferrer'><h5 class='card-title text-white'>" + repoName + "</h5></a><p class='card-text mt-2'>" + repoDescription + "</p></div></div></div>"
+                );
+                i++;
+            }
         }
     });
 });
