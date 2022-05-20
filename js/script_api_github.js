11a4bf050892a42cce486d19b89701a5cd8a1918
Ahmosys <ahmosyspro@protonmail.com>
2022-05-20 14:58:10 +0200

    ✨ Edited loop for number of cards display

--- js/script_api_github.js
+++ js/script_api_github.js
@@ -35,6 +35,8 @@ $(document).ready(function() {
                     );
                     j+=1;
                 }
+            } else {
+                break;
             }
         }
     });
